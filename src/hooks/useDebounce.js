935202b360085c8ec705c190e72d397886de61f6
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-06-19 14:41:51 +0200

    refactor: Migliora la leggibilità del codice nel hook useDebounce

--- src/hooks/useDebounce.js
+++ src/hooks/useDebounce.js
@@ -3,7 +3,9 @@ import { useRef, useCallback } from "react";
 export default function useDebounce(callback, delay) {
   let timer = useRef(null); //senza useRef questa variabile viene inizializzata ad ogni render
 
-  return useCallback( //Senza useCallback avrei una funzione instabile, mentre così il valore viene ricalcolata solo se cambiano le dipendenze
+  return useCallback(
+    /* Senza useCallback avrei una funzione instabile, 
+   mentre così il valore viene ricalcolato solo se cambiano le dipendenze */
     (value) => {
       if (timer.current) {
         clearTimeout(timer.current);
